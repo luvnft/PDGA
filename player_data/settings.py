97c3820a18050d99b62e677b10bc6a45561fb791
Chad Woodard <chadwoodard78@gmail.com>
2024-07-11 09:52:51 -0500

    Update settings to include onrender.com as a host

--- player_data/settings.py
+++ player_data/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-7oy^fd*q!7)l$h$dxtcs0_8uxciy1^zeybj(ldof&fs#_elfvg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['player_data.onrender.com',
+ALLOWED_HOSTS = ['pdga-player-data.onrender.com',
+                 'player_data.onrender.com',
                 'localhost',
                 '127.0.0.1']
 
