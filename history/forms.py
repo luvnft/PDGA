c76d8b1a9b47e7abd6da2abc6b8d9eda270ee1d2
Chad Woodard <chadwoodard78@gmail.com>
2024-07-12 10:26:18 -0500

    Set search limit to 8, more styling changes, updated FAQ

--- history/forms.py
+++ history/forms.py
@@ -3,6 +3,16 @@ from django import forms
 class PlayerNumberForm(forms.Form):
     player_number = forms.CharField(
         label='Player Numbers',
-        widget=forms.TextInput(attrs={'placeholder': 'Enter player PDGA numbers separated by commas'}),
-        max_length=1000
+        widget=forms.TextInput(attrs={'placeholder': 'Enter up to 8 player PDGA numbers separated by commas'}),
+        max_length=70
     )
+
+    def clean_player_number(self):
+        data = self.cleaned_data['player_number']
+        player_numbers = [num.strip() for num in data.split(',')]
+        
+        # Limit the number of player numbers to 8
+        if len(player_numbers) > 8:
+            raise forms.ValidationError("You can only search for up to 8 player numbers at a time.")
+        
+        return data
